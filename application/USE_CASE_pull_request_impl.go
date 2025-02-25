38f49444af8ad01873d65ee33ec424c4dab12877
Hector <orantesroman39@gmail.com>
2025-02-25 11:37:12 -0600

    Test-3

--- application/USE_CASE_pull_request_impl.go
+++ application/USE_CASE_pull_request_impl.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	domain "github_wb/domain/value_objects"
 	"log"
-		"fmt"
+	"fmt"
 )
 
 func ProcessPullRequest(payload []byte) int {
@@ -25,38 +25,31 @@ func ProcessPullRequest(payload []byte) int {
 		log.Printf("Pull Request Action no es Closed: %s", eventPayload.Action)
 	}
 
-	log.Printf("Evento Pull Request recibido: Acción=%s, Título='%s', Base='%s', Repositorio='%s'",
+
+	log.Printf("Evento Pull Request recibido: Acción=%s, Título='%s', Base='%s', Repositorio='%s', Cambio='%s', Rama='%s'",
 		eventPayload.Action,
 		eventPayload.PullRequest.Title,
 		eventPayload.PullRequest.Base.Ref,
-		eventPayload.Repository.FullName)
-
-	
-
+		eventPayload.Repository.FullName,
+		eventPayload.PullRequest.Title, // Aquí puedes incluir la descripción del cambio
+		eventPayload.PullRequest.Head.Ref) // Rama del cambio
 
 	if err := json.Unmarshal(payload, &eventPayload); err != nil {
 		log.Printf("Error al procesar el payload: %v", err)
 		return 500
 	}
 
-	
-	log.Printf("Evento Pull Request recibido: Acción=%s, Título='%s', Base='%s', Repositorio='%s'",
-		eventPayload.Action,
-		eventPayload.PullRequest.Title,
-		eventPayload.PullRequest.Base.Ref,
-		eventPayload.Repository.FullName)
 
 	switch eventPayload.Action {
 	case "opened":
-		fmt.Printf("\n Nuevo Pull Request creado por %s en %s\n", eventPayload.PullRequest.User.Login, eventPayload.Repository.FullName)
+		fmt.Printf("\nNuevo Pull Request creado por %s en %s\n", eventPayload.PullRequest.User.Login, eventPayload.Repository.FullName)
 	case "synchronize":
-		fmt.Printf("\n Pull Request actualizado: %s\n", eventPayload.PullRequest.Title)
+		fmt.Printf("\nPull Request actualizado: %s\n", eventPayload.PullRequest.Title)
 	case "closed":
-		fmt.Printf("\n Pull Request cerrado: %s\n", eventPayload.PullRequest.Title)
+		fmt.Printf("\nPull Request cerrado: %s\n", eventPayload.PullRequest.Title)
 	default:
-		fmt.Printf("\n  !Acción del PR detectada: %s\n", eventPayload.Action)
+		fmt.Printf("\n¡Acción del PR detectada: %s\n", eventPayload.Action)
 	}
-
 
 	return 200
 }
